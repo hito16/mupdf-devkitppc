5a50e19a701228536ba6043d87709bd039563dc8
hito16 <16582909+hito16@users.noreply.github.com>
2025-06-07 18:04:50 -0700

    set HAVE_OBJCOPY=no

--- make_wiiu.sh
+++ make_wiiu.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+cd /project/mupdf-devkitppc
+
+pwd
+
+
+worked_but_big(){
 make clean
 make \
 CC=$DEVKITPRO/devkitPPC/bin/powerpc-eabi-gcc \
 CXX=$DEVKITPRO/devkitPPC/bin/powerpc-eabi-g++ \
+AR="$DEVKITPRO/devkitPPC/bin/powerpc-eabi-ar" \
+LD="$DEVKITPRO/devkitPPC/bin/powerpc-eabi-ld" \
 CFLAGS="-I./include -O2 -ffunction-sections -fdata-sections -DESPRESSO -mcpu=750 -meabi -mhard-float -D__WIIU__ -D__WUT__ -DBIGENDIAN -DFZ_ENABLE_BARCODE=0 -D_GNU_SOURCE -DCMS_NO_PTHREADS" \
 CXXFLAGS="-I./include -O2 -ffunction-sections -fdata-sections -DESPRESSO -mcpu=750 -meabi -mhard-float -D__WIIU__ -D__WUT__ -DBIGENDIAN -DFZ_ENABLE_BARCODE=0 -D_GNU_SOURCE -DCMS_NO_PTHREADS" \
 HAVE_X11=no \
 HAVE_GL=no \
 HAVE_GLUT=no \
 HAVE_CURL=no \
-HAVE_JS=no \
-HAVE_SVG=no \
-HAVE_HTML=no \
-HAVE_LCMS2=no \
-NO_TOOLS=yes \
-HAVE_TEXT_OUTPUT=no \
-NO_OCRLIB=yes \
+HAVE_OBJCOPY=no \
+HAVE_HARFBUZZ=yes \
+html=yes \
+shared=no \
+threading=no \
+HARFBUZZ_SRC= \
+FREETYPE_SRC= \
+XCFLAGS="$($DEVKITPRO/portlibs/wiiu/bin/powerpc-eabi-pkg-config --cflags harfbuzz freetype2)" \
+XLIBS="$($DEVKITPRO/portlibs/wiiu/bin/powerpc-eabi-pkg-config --libs harfbuzz freetype2)" \
 build/release/libmupdf.a \
 build/release/libmupdf-third.a
 
+}
+
+worked_but_big
